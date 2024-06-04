dd424156f9c58203ce530a21308cd4d8d2d12cc8
Paul Leroy <paul.leroy@univ-rennes1.fr>
2024-06-04 15:16:06 +0200

    Update .readthedocs.yaml

--- docs/conf.py
+++ docs/conf.py
@@ -20,9 +20,25 @@ templates_path = ['_templates']
 exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 
 
+# Add any Sphinx extension module names here, as strings. They can be
+# extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
+# ones.
+extensions = [
+    'sphinx_rtd_theme',
+    'sphinx.ext.githubpages',
+    'sphinx.ext.autodoc',
+    'sphinx.ext.autosummary',
+    'sphinx.ext.todo',
+    'sphinx.ext.mathjax',
+    'sphinx.ext.viewcode',
+    'sphinx.ext.napoleon',
+    'sphinx.ext.intersphinx',
+    'sphinx.ext.doctest',
+    # 'sphinx_autodoc_typehints'
+]
 
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-html_theme = 'alabaster'
+html_theme = "sphinx_rtd_theme"
 html_static_path = ['_static']
--- docs/index.rst
+++ docs/index.rst
@@ -9,6 +9,8 @@ Welcome to lidar_platform's documentation!
 .. toctree::
    :maxdepth: 2
    :caption: Contents:
+   
+   ../tools
 
 
 
